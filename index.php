b90fcefed18bc279a5c8f47365880e4205c91036
Paul Mastavy <gammabeam82@gmail.com>
2018-07-15 14:04:27 +0300

    Redirect GET requests.

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Gammabeam82\Bot\DependencyInjection\AppContainerBuilder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 if (false === isset($_SERVER['APP_ENV'])) {
     (new \Symfony\Component\Dotenv\Dotenv())->load(sprintf("%s/.env", __DIR__));
+}
+
+$request = Request::createFromGlobals();
+
+if(false !== $request->isMethod('GET')) {
+    return new RedirectResponse(sprintf("https://t.me/%s", getenv('BOTNAME')));
 }
 
 $container = (new AppContainerBuilder())->getContainer();
